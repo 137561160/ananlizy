a211a1b33473ab23b745f42df260202b7b2c5750
137561160 <61008555+137561160@users.noreply.github.com>
2026-04-08 16:53:21 +0800

    Update tes.py

--- tes.py
+++ tes.py
@@ -1,7 +1,7 @@
 网关： 深圳市秋实物联 ESP32 RS485 隔离版导轨网关 (￥130)
 电表： 安科瑞 ADL400  ADL400/FC 带RS485通讯+尖峰平谷(￥460+) https://detail.tmall.com/item.htm?abbucket=5&id=641303594649 
-传感器： 正泰 NCTK24 开口式互感器 (3只*46) NCTK-24 100A/5A 1.5m黄 正泰开口式电流 https://item.taobao.com/item.htm?abbucket=5&id=947456322960
-电源： 明纬导轨电源 HDR-15-12 (￥30)
+传感器： 正泰 NCTK24 开口式互感器 (3只*￥46) NCTK-24 100A/5A 1.5m黄 正泰开口式电流 https://item.taobao.com/item.htm?abbucket=5&id=947456322960
+电源： 明纬导轨电源 HDR-15-12 (￥30) HDR-15-24 https://detail.tmall.com/item.htm?abbucket=5&id=741103231346
 信号： 2.4G 磁吸天线 + IPEX转SMA跳线 (￥15)
 线材： 双绞屏蔽线 (1-3米) (￥5)
 --------------------------------------------------------
