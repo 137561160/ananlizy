d66ba2460a531a6c5b424a3d576ef23a64568139
137561160 <61008555+137561160@users.noreply.github.com>
2026-04-08 16:58:49 +0800

    Update tes.py

--- tes.py
+++ tes.py
@@ -2,8 +2,8 @@
 电表： 安科瑞 ADL400  ADL400/FC 带RS485通讯+尖峰平谷(￥460+) https://detail.tmall.com/item.htm?abbucket=5&id=641303594649 
 传感器： 正泰 NCTK24 开口式互感器 (3只*￥46) NCTK-24 100A/5A 1.5m黄 正泰开口式电流 https://item.taobao.com/item.htm?abbucket=5&id=947456322960
 电源： 明纬导轨电源 HDR-15-12 (￥30) HDR-15-24 https://detail.tmall.com/item.htm?abbucket=5&id=741103231346
-信号： 2.4G 磁吸天线 + IPEX转SMA跳线 (￥15)
-线材： 双绞屏蔽线 (1-3米) (￥5)
+信号： 2.4G 磁吸天线 + IPEX转SMA跳线 (￥15)【Z款】8DB/高度29CM【2.4G】 2.4G/WIFI/蓝牙 https://item.taobao.com/item.htm?id=646695506773
+线材： 双绞屏蔽线 (1-3米) (￥5) 双绞屏蔽 2芯X0.5平方 1.5米 https://item.taobao.com/item.htm?id=636265473315
 --------------------------------------------------------
 网关： 深圳市秋实物联 ESP32 RS485 隔离版导轨网关 (￥130)
 电表： 正泰 DTSU666 (1.5(6)A版) (￥220+)
